a486a2e1401878d7f0fa73541f71c3caffd035d5
mydss-dev <180769754+mydss-dev@users.noreply.github.com>
2026-09-07 20:47:29 +0800

    Brand Windows TUN interface as Litchi

--- lib/config/app_identity.dart
+++ lib/config/app_identity.dart
@@ -1,7 +1,8 @@
 abstract final class AppIdentity {
   static const storageKey = 'litchi';
   static const storageDirectoryName = 'Litchi';
-  static const tunInterfaceAlias = 'TUN-LOCAL';
+  static const tunInterfaceAlias = 'Litchi';
+  static const legacyTunInterfaceAlias = 'TUN-LOCAL';
   static const instancePing = 'LITCHI_FOCUS_V1:litchi';
   static const instancePong = 'LITCHI_FOCUS_OK_V1:litchi';
   static const instanceLockPort = 54891;
