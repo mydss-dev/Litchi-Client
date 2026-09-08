0405807620916f8544b44c9161315312dc58b1fd
mydss-dev <180769754+mydss-dev@users.noreply.github.com>
2026-09-08 09:19:20 +0800

    Remove unused legacy TUN alias

--- lib/config/app_identity.dart
+++ lib/config/app_identity.dart
@@ -2,7 +2,6 @@ abstract final class AppIdentity {
   static const storageKey = 'litchi';
   static const storageDirectoryName = 'Litchi';
   static const tunInterfaceAlias = 'Litchi';
-  static const legacyTunInterfaceAlias = 'TUN-LOCAL';
   static const instancePing = 'LITCHI_FOCUS_V1:litchi';
   static const instancePong = 'LITCHI_FOCUS_OK_V1:litchi';
   static const instanceLockPort = 54891;
