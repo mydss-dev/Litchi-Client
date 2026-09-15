0b503a83d8ceb89dedba6ef7b0ad25d3b41f4848
mydss-dev <180769754+mydss-dev@users.noreply.github.com>
2026-09-16 00:35:29 +0800

    feat(v3): enforce 900x700 desktop workspace

---
+++ lib/v3/app/v3_window_bootstrap.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+class V3WindowBootstrap extends StatefulWidget {
+  const V3WindowBootstrap({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  State<V3WindowBootstrap> createState() => _V3WindowBootstrapState();
+}
+
+class _V3WindowBootstrapState extends State<V3WindowBootstrap> {
+  bool _synced = false;
+
+  bool get _desktop => Platform.isWindows || Platform.isMacOS || Platform.isLinux;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_synced || !_desktop) return;
+    _synced = true;
+    WidgetsBinding.instance.addPostFrameCallback((_) => _syncV3Window());
+  }
+
+  Future<void> _syncV3Window() async {
+    try {
+      await windowManager.setMinimumSize(const Size(760, 600));
+      await windowManager.setSize(const Size(900, 700));
+      await windowManager.center();
+    } catch (_) {
+      // window_manager can be absent in widget tests.
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+}
